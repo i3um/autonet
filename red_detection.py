70e78a0e7262e9a555a4b5219db2688538a8da0b
Ilya Borisov <47271731+i3um@users.noreply.github.com>
2020-09-29 18:45:24 +0300

    Update red_detection.py

--- red_detection.py
+++ red_detection.py
@@ -52,4 +52,31 @@ while(True):
             img.draw_circle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
     print(clock.fps())  
+###################################################
+import sensor, image, time, math
+
+
+thresholds = [(30, 100, 15, 127, 15, 127)] 
+              
+
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False) 
+sensor.set_auto_whitebal(False) 
+clock = time.clock()
+
+
+while(True):
+    clock.tick()
+    img = sensor.snapshot()
+    for blob in img.find_blobs(thresholds, pixels_threshold=200, area_threshold=200):
+        if blob.elongation() > 0.5:
+            img.draw_circle()
+            img.draw_edges(blob.min_corners(), color=(255,0,0))
+    print(clock.fps())
+        
+      
  
